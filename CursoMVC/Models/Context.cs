85b561082bfcf897683a96473c32d796e8b1014e
gabrielpaulino <gabriel.paulino104@gmail.com>
2020-01-23 16:37:57 -0300

    Projeto do Curso ASP NET MVC - Digital Innovation One

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        //Criando Tabela Categoria <Tipo> nomeTabela: Categorias
+        //Muito importante inserir o "virtual" para que funcione os tests
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+
+    }
+}
